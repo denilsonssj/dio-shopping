f9cdc45d567563ff762c0a65a2a9d9d1b1292edd
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2022-01-21 16:31:41 -0300

    Add initial frontend structure

---
+++ frontend/src/index.tsx
@@ -0,0 +1,9 @@
+import { Fragment, StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <StrictMode>
+    <Fragment />
+  </StrictMode>,
+  document.getElementById('wrapper') as HTMLElement,
+);
