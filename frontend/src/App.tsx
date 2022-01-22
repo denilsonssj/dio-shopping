b32bddbe1c4c7b7d272806a36b8b2c8beb883f5e
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2022-01-22 18:04:50 -0300

    Add initial setup to react-redux

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { Provider as ReduxProvider } from 'react-redux';
 
 import { Pages } from '~/components/Pages';
 import { theme } from '~/styles/theme';
+import { store } from '~/store';
 
 export const App = () => {
     return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Pages />
-        </ThemeProvider>
+        <ReduxProvider store={store}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Pages />
+            </ThemeProvider>
+        </ReduxProvider>
     );
 }
