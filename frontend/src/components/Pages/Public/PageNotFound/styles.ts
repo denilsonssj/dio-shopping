da2d7d5d455341d007477b66c72f2f61d9d04abe
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2022-01-22 20:34:44 -0300

    Add not found page and add svg type declaration

---
+++ frontend/src/components/Pages/Public/PageNotFound/styles.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+import { Link as RouterLink } from '~/components/Shared/Link';
+
+export const Image = styled('img')`
+    width: 100%;
+    height: auto;
+    max-width: 800px;
+    padding: 10px;
+`;
+
+export const Link = styled(RouterLink)`
+    color: black;
+`;
--- frontend/src/components/Pages/Public/index.tsx
+++ frontend/src/components/Pages/Public/index.tsx
@@ -3,12 +3,14 @@ import {  Route } from 'react-router-dom';
 import { MainLayout } from '~/components/Layouts/Main';
 import { Home } from './Home';
 import { Contact } from './Contact';
+import { PageNotFound } from './PageNotFound';
 
 export const Public = () => {
     return (
         <MainLayout>
             <Route path='/' element={<Home />} />
             <Route path='/contact' element={<Contact />} />
+            <Route path='*' element={<PageNotFound />} />
         </MainLayout>
     );
 }
