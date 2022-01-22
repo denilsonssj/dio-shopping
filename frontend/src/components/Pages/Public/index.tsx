17eee33598e3cffd212c3a31739e15db5e35cbab
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2022-01-22 17:52:11 -0300

    Update contact page layout

--- frontend/src/components/Pages/Public/index.tsx
+++ frontend/src/components/Pages/Public/index.tsx
@@ -1,12 +1,14 @@
 import {  Route } from 'react-router-dom';
-import { MainLayout } from '~/components/Layouts/Main';
 
+import { MainLayout } from '~/components/Layouts/Main';
 import { Home } from './Home';
+import { Contact } from './Contact';
 
 export const Public = () => {
     return (
         <MainLayout>
             <Route path='/' element={<Home />} />
+            <Route path='/contact' element={<Contact />} />
         </MainLayout>
     );
 }
