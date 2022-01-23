2e4c1e8858ddb438099935134c97bee7f7f99404
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2022-01-23 20:44:15 -0300

    Add api url enviroment variable to settings file

--- frontend/src/settings.ts
+++ frontend/src/settings.ts
@@ -1,2 +1,3 @@
 export const ENV = (process.env.REACT_APP_ENV as string) || 'production';
 export const IS_DEVELOPMENT = ENV === 'development';
+export const API_URL = (process.env.REACT_APP_API_URL as string) || '';
