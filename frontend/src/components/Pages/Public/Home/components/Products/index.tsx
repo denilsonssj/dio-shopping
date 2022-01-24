254227c2fd1c6fe9d582ed8a124a75586acfdf92
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2022-01-24 09:24:47 -0300

    Update products component

--- frontend/src/components/Pages/Public/Home/components/Products/index.tsx
+++ frontend/src/components/Pages/Public/Home/components/Products/index.tsx
@@ -1,16 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Container, Grid } from '@mui/material';
 
+import { api } from '~/services/api';
 import { IProduct } from '~/interfaces/Product';
+import { API_URL } from '~/settings';
 import { Product } from '../Product';
 
 export const Products = () => {
 
-    const [products] = useState<IProduct[]>([]);
+    const [products, setProducts] = useState<IProduct[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const { data } = await api.get(`${API_URL}/products`);
+            setProducts(data);
+        }
+        fetchData();
+    }, []);
 
     return (
         <Container sx={{ marginTop: 8 }}>
-            <Grid container spacing={4}>
+            <Grid container spacing={6}>
                 {
                     products && products.length > 0 ? (
                         products.map(({ id, title, category, description, price, imageUrl, rating }: IProduct) => (
