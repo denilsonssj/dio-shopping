5091228e45c8bf710d6fbf42786cda52592625b8
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2022-01-24 20:50:54 -0300

    Add initial backend

---
+++ backend/src/app.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3333, () => console.log('Listening on port 3333'));
