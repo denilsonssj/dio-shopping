55a2653798dd039ceb9c7e732d946bc87accdb86
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2022-01-26 09:48:56 -0300

    Add new database enviroment variables and fix typeorm connection

--- backend/ormconfig.ts
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    cli: {
-        migrationsDir: __dirname + '/src/database/migrations',
-        entitiesDir: __dirname + '/src/entities',
-    },
-    migrations: [__dirname + '/src/database/migrations/*.ts'],
-    entities: [__dirname  + '/src/entities/*.ts'],
-}
