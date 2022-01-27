f88abff8fcf22cf60352bf2c75fde1f003c339a6
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2022-01-27 10:50:19 -0300

    Update header component add new buttons to navigations

--- frontend/src/components/Layouts/Main/components/Header/index.tsx
+++ frontend/src/components/Layouts/Main/components/Header/index.tsx
@@ -1,9 +1,42 @@
-import { AppBar, Box, IconButton, Toolbar } from '@mui/material';
+import { ReactNode } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AppBar, Box, IconButton, Toolbar, Button } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import HomeIcon from '@mui/icons-material/Home';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ContactMailIcon from '@mui/icons-material/ContactMail';
 
 import { Link } from '~/components/Shared/Link';
 
+
+interface LinkProps {
+    path: string;
+    icon: ReactNode;
+    label: string;
+}
+
 export const Header = () => {
+
+    const navigate = useNavigate();
+
+    const links: LinkProps[] = [
+        {
+            path: '/',
+            icon: <HomeIcon />,
+            label: 'Home'
+        },
+        {
+            path: '/cart',
+            icon: <ShoppingCartIcon />,
+            label: 'Cart'
+        },
+        {
+            path: '/contact',
+            icon: <ContactMailIcon />,
+            label: 'Contact'
+        }
+    ];
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
@@ -22,9 +55,18 @@ export const Header = () => {
                             DIO Shopping
                         </Link>
                     </Box>
-                    <Link to="/">Home</Link>
-                    <Link to="/cart">Cart</Link>
-                    <Link to="/contact">Contact</Link>
+                    {
+                        links && links.map((link: LinkProps) => (
+                            <Button
+                                key={link.path}
+                                startIcon={link.icon}
+                                sx={{ color: 'white', textTransform: 'capitalize' }}
+                                onClick={() => navigate(link.path)}
+                            >
+                                {link.label}
+                            </Button>
+                        ))
+                    }
                 </Toolbar>
             </AppBar>
         </Box>
