1929d220a15f968b2173efddeb3ffbbee86058cc
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2022-01-28 09:58:26 -0300

    Add cart context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
+import { CartProvider } from '~/contexts/CartContext';
 import { Pages } from '~/components/Pages';
 import { theme } from '~/styles/theme';
 
 export const App = () => {
     return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Pages />
-        </ThemeProvider>
+        <CartProvider>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Pages />
+            </ThemeProvider>
+        </CartProvider>
     );
 }
