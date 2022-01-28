2f2432c99d85656ddf04ee9fff5a1d497f55774f
Denilson Santos Sobrinho Júnior <dssjunior98@gmail.com>
2022-01-28 09:48:55 -0300

    Remove redux dependency and update app definition

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,14 @@
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { Provider as ReduxProvider } from 'react-redux';
 
 import { Pages } from '~/components/Pages';
 import { theme } from '~/styles/theme';
-import { store } from '~/store';
 
 export const App = () => {
     return (
-        <ReduxProvider store={store}>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <Pages />
-            </ThemeProvider>
-        </ReduxProvider>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Pages />
+        </ThemeProvider>
     );
 }
